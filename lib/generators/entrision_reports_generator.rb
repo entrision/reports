c0f1abfd35102dff520b6c402c9852352875873a
Robert D. Cotey II <rcotey@entrision.com>
2021-10-22 16:27:54 -0400

    this should have the generator working

---
+++ lib/generators/entrision_reports_generator.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails/generators'
+
+module EntrisionReports
+
+    class InstallGenerator < Rails::Generators::Base
+      desc 'Install Entrision Reports Files'
+      def install
+        puts 'Installing Things'
+        # TODO: copy admin/reports.rb to project as a template
+      end
+    end
+
+end
+
