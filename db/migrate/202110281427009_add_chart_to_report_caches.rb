8c69fb9b6c423b70c91cfa57074eb6f52189497c
Robert D. Cotey II <rcotey@entrision.com>
2021-11-08 11:16:41 -0500

    Fix migration on existing situations

--- db/migrate/202110281427009_add_chart_to_report_caches.rb
+++ db/migrate/202110281427009_add_chart_to_report_caches.rb
@@ -1,5 +1,7 @@
 class AddChartToReportCaches < ActiveRecord::Migration[6.0]
   def change
-    add_column :report_caches, :chart, :json
+    unless column_exists? :report_caches, :chart
+      add_column :report_caches, :chart, :json
+    end
   end
 end
